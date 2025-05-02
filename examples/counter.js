16ea0516d842d04db7c507c72aece7727e1a7ba7
Adwait Arajaria <adeveloper121@gmail.com>
2025-05-02 18:56:33 +0530

    composability: added builder methods forge, text, on

---
+++ examples/counter.js
@@ -0,0 +1,29 @@
+import { createSignal, createEffect, createMemo } from "../signal.js";
+import { forge } from "../compose.js";
+
+const body = document.body;
+
+function Counter() {
+    const count = createSignal(0);
+    const double = createMemo(() => count.value * 2);
+    
+    return forge("div")
+        .children([
+        forge("h1")
+            .text(() => count.value)
+            .build(),
+        forge("button")
+            .on("click", () => count.value += 1)
+            .text('+1')
+            .build(),
+        forge("button")
+            .on("click", () => count.value -= 1)
+            .text('-1')
+            .build(),
+        forge("h2")
+            .text(() => double.value)
+            .build(),
+        ]).build();
+}
+
+body.appendChild(Counter());
