05cdde6e913d189cd43bd72eec5ed5ece79edb14
azatshtru <adeveloper121@gmail.com>
2025-05-04 00:03:31 +0530

    composability: added styling, made children fine-grained reactive, added attributes and properties

--- compose.js
+++ compose.js
@@ -23,11 +23,53 @@ export function forge(htmlTag) {
         },
 
         children(children) {
-            createEffect(() => {
-                this.element.replaceChildren();
-                this.element.append(...children);
-            });
+            this.element.replaceChildren();
+            for(const child of children) {
+                if(typeof child === 'function') {
+                    const reactiveElement = document.createElement('div');
+                    reactiveElement.style.all = 'unset';
+                    createEffect(() => reactiveElement.replaceChildren(...Array.isArray(child()) ? child() : [child()]));
+                    this.element.append(reactiveElement);
+                } else {
+                    this.element.append(child);
+                }
+            }
             return this;
-        }
+        },
+
+        attr(attributes) {
+            for(const [key, value] of Object.entries(attributes)) {
+                if(typeof value === 'function') {
+                    createEffect(() => this.element.setAttribute(key, value()));
+                } else {
+                    this.element.setAttribute(key, value);
+                }
+            }
+            return this;
+        },
+
+        property(key, value) {
+            if(typeof value === 'function') {
+                createEffect(() => this.element[key] = value());
+            } else {
+                this.element[key] = value;
+            }
+            return this;
+        },
+
+        style(style) {
+            if(typeof style === 'function') {
+                createEffect(() => {
+                    this.element.removeAttribute('style');
+                    Object.assign(this.element.style, style());
+                });
+            } else {
+                Object.assign(this.element.style, style);
+            }
+            return this;
+        },
     };
+}
+
+const cast = {
 }
---
+++ examples/spellcast.js
@@ -0,0 +1,45 @@
+import { createSignal, createEffect, createMemo } from "../signal.js";
+import { forge } from "../compose.js";
+
+function SpellcastHistory() {
+    const spellThreshold = 5;
+    const history = createSignal([]);    
+    const style = createMemo(() => history.value.length > spellThreshold ? 
+        { background: 'black', color: 'white' } : { background: 'white', color: 'black' });
+
+    const castSpell = (spell) => history.value = [...history.value, spell];
+
+    return forge("div")
+        .children([
+            Spellcast(castSpell),
+            () => history.value.length > spellThreshold ? forge("h2").text('You have cast too much spells young wizard').build() : '',
+            () => history.value.map(x => forge("p").text(x).build()),
+            forge('button').text('forget spellcast history').on('click', () => history.value = []).build(),
+        ])
+        .style(() => style.value)
+        .build();
+}
+
+function Spellcast(castSpell) {
+    const spell = createSignal("");
+    
+    return forge("div")
+        .children([
+        forge("input")
+            .property('value', () => spell.value)
+            .attr({
+                placeholder: 'cast spell',
+            })
+            .on("keydown", e => {
+                if(e.key == "Enter") {
+                    castSpell(spell.value);
+                    spell.value = "";
+                }
+            })
+            .on("input", e => spell.value = e.target.value)
+            .build(),
+        ]).build();
+}
+
+const body = document.body;
+body.appendChild(SpellcastHistory());
